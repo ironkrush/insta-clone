5b4bd6c3990d3741f9add909ed953881eab32884
ironkrush <krush2752@gmail.com>
2024-02-21 20:17:51 +0530

    ffirst commit

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/instainsta");
+mongoose.connect("mongodb+srv://krush2752:R8dDZaWYsZBEAeL9@cluster0.8lbifpw.mongodb.net");
 
 const userSchema = mongoose.Schema({
   username: String,
